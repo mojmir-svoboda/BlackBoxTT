c23249f30064786b8eb155baf4522bc8411ab142
Mojmir Svoboda <mojmir.svoboda@bistudio.com>
2016-07-22 13:20:05 +0200

    * logging build (still not working properly)

--- 3rd_party/logging/build_all_vs2015.bat
+++ 3rd_party/logging/build_all_vs2015.bat
@@ -7,12 +7,13 @@ set PATH=C:\Program Files (x86)\CMake\bin;C:\Program Files\CMake\bin;%PATH%
 
 set MSCVER=14
 
-set INSTDIR=vs_64
+set INSTDIR=%CD%\vs14_x64
 mkdir _projects.vs%MSCVER%.64
 cd _projects.vs%MSCVER%.64
+
 call "C:\Program Files (x86)\Microsoft Visual Studio %MSCVER%.0\VC\vcvarsall.bat" x86_amd64
 if %errorlevel% neq 0 goto TERM
-cmake -G "Visual Studio %MSCVER% Win64" -DCMAKE_INSTALL_PREFIX:PATH=%INSTDIR% ..
+cmake -G "Visual Studio %MSCVER% Win64" -DOPTION_BUILD_SHARED=ON -DCMAKE_INSTALL_PREFIX:PATH=%INSTDIR% ..
 if %errorlevel% neq 0 goto TERM
 devenv logging.sln /build RelWithDebInfo /project INSTALL
 devenv logging.sln /build Debug /project INSTALL
@@ -20,12 +21,12 @@ if %errorlevel% neq 0 goto TERM
 
 cd ..
 
-set INSTDIR=c:/_builds/vs_32
+set INSTDIR=%CD%\vs14_32
 mkdir _projects.vs%MSCVER%.32
 cd _projects.vs%MSCVER%.32
 call "C:\Program Files (x86)\Microsoft Visual Studio %MSCVER%.0\VC\vcvarsall.bat" x86
 if %errorlevel% neq 0 goto TERM
-cmake -G "Visual Studio %MSCVER%" -DCMAKE_INSTALL_PREFIX:PATH=%INSTDIR% ..
+cmake -G "Visual Studio %MSCVER%" -DOPTION_BUILD_SHARED=ON -DCMAKE_INSTALL_PREFIX:PATH=%INSTDIR% ..
 if %errorlevel% neq 0 goto TERM
 devenv logging.sln /build RelWithDebInfo /project INSTALL
 devenv logging.sln /build Debug /project INSTALL
