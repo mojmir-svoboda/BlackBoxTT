e75f35f274438d952e68312d46926e6fdb15099e
Mojmir Svoboda <mojmir.svoboda@flogging.berlios.de>
2016-09-15 21:48:52 +0200

    * new trace proto

--- bblib/logging.cpp
+++ bblib/logging.cpp
@@ -19,38 +19,68 @@ void initTrace (char const * appName, char const * addr, char const * port)
 
 	//TRACE_SET_LEVEL(CTX_NET_MESSAGE | CTX_NETWORK, all_lvl);
 
-	trace::DictionaryPair lvl_dict[]{
-		{ (1u << LL_VERBOSE), "verbose" },
-		{ (1u << LL_DEBUG), "dbg" },
-		{ (1u << LL_INFO), "nfo" },
-		{ (1u << LL_WARNING), "WARN" },
-		{ (1u << LL_ERROR), "ERROR" },
-		{ (1u << LL_FATAL), "FATAL" },
+	trace::level_t lvl_dict_values[] = {
+		(1u << LL_VERBOSE),
+		(1u << LL_DEBUG),
+		(1u << LL_INFO),
+		(1u << LL_WARNING),
+		(1u << LL_ERROR),
+		(1u << LL_FATAL)
 	};
-	static_assert(sizeof(lvl_dict) / sizeof(*lvl_dict) == e_max_trace_level, "arrays do not match");
-	TRACE_SET_LEVEL_DICTIONARY(lvl_dict, sizeof(lvl_dict) / sizeof(*lvl_dict));
+	static_assert(sizeof(lvl_dict_values) / sizeof(*lvl_dict_values) == e_max_trace_level, "array do not match enum");
+	char const * lvl_dict_names[] = {
+		"vrbs",
+		"dbg",
+		"nfo",
+		"WARN",
+		"ERROR",
+		"FATAL"
+	};
+	static_assert(sizeof(lvl_dict_names) / sizeof(*lvl_dict_names) == e_max_trace_level, "array do not match enum");
+	TRACE_SET_LEVEL_DICTIONARY(lvl_dict_values, lvl_dict_names, sizeof(lvl_dict_values) / sizeof(*lvl_dict_values));
 
-	trace::DictionaryPair ctx_dict[] = {
-		{ CTX_DEFAULT 			,  "dflt" },
-		{ CTX_INIT 					,  "Init" },
-		{ CTX_BB						,  "BB" },
-		{ CTX_GFX 					,  "Gfx" },
-		{ CTX_EXPLORER 			,  "Explr" },
-		{ CTX_HOOK  				,  "Hook" },
-		{ CTX_TRAY 					,  "Tray" },
-		{ CTX_CONFIG				,  "Cfg" },
-		{ CTX_SCRIPT				,  "Script" },
-		{ CTX_STYLE					,  "Style" },
-		{ CTX_PROFILING 		,  "Prof" },
-		{ CTX_SERIALIZE 		,  "Ser" },
-		{ CTX_MEMORY 				,  "Memory" },
-		{ CTX_RESOURCES			,  "Rsrc" },
-		{ CTX_BBLIB					,  "Lib" },
-		{ CTX_BBLIBCOMPAT		,  "LibCompat" },
-		{ CTX_PLUGINMGR			,  "PlugMgr" },
-		{ CTX_PLUGIN 				,  "Plug" },
+	trace::context_t ctx_dict_values[] = {
+		CTX_DEFAULT 			,
+		CTX_INIT 					,
+		CTX_BB						,
+		CTX_GFX 					,
+		CTX_EXPLORER 			,
+		CTX_HOOK  				,
+		CTX_TRAY 					,
+		CTX_CONFIG				,
+		CTX_SCRIPT				,
+		CTX_STYLE					,
+		CTX_PROFILING 		,
+		CTX_SERIALIZE 		,
+		CTX_MEMORY 				,
+		CTX_RESOURCES			,
+		CTX_BBLIB					,
+		CTX_BBLIBCOMPAT		,
+		CTX_PLUGINMGR			,
+		CTX_PLUGIN
 	};
-	TRACE_SET_CONTEXT_DICTIONARY(ctx_dict, sizeof(ctx_dict) / sizeof(*ctx_dict));
+	char const * ctx_dict_names[] = {
+		"dflt",
+		"Init",
+		"BB",
+		"Gfx",
+		"Explr",
+		"Hook",
+		"Tray",
+		"Cfg",
+		"Script",
+		"Style",
+		"Prof",
+		"Ser",
+		"Memory",
+		"Rsrc",
+		"Lib",
+		"LibCompat",
+		"PlugMgr",
+		"Plug"
+	};
+	static_assert(sizeof(ctx_dict_values) / sizeof(*ctx_dict_values) == sizeof(ctx_dict_names) / sizeof(*ctx_dict_names), "arrays do not match");
+	TRACE_SET_CONTEXT_DICTIONARY(ctx_dict_values, ctx_dict_names, sizeof(ctx_dict_values) / sizeof(*ctx_dict_values));
 
 	// 2) connect
 	TRACE_CONNECT(addr, port);
