c4cf02e918ad82123511e108aacd96af6340c670
Mojmir Svoboda <mojmir.svoboda@bistudio.com>
2017-02-10 15:11:01 +0100

    * move widget window

--- blackbox/gfx/GuiWidget.h
+++ blackbox/gfx/GuiWidget.h
@@ -15,12 +15,13 @@ namespace bb
 		virtual ~GuiWidget () { }
 		virtual void DrawUI () = 0;
 		virtual wchar_t const * GetWidgetTypeName () = 0;
+		virtual bbstring const & GetId () const = 0;
 		virtual bool loadConfig (YAML::Node & y_cfg_node) = 0;
 		//virtual bool saveConfig (YAML::Node & y_cfg_node) = 0;
-		virtual bbstring const & GetId () const = 0;
 
 		virtual void Show (bool on) = 0;
 		virtual bool Visible () const = 0;
+		virtual void MoveWindow (int x, int y) { m_gfxWindow->MoveWindow(x, y); }
 	};
 }
 
